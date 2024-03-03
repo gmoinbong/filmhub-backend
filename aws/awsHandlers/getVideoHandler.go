60d20faa674b72b0c8aa5d3f5bb7cf053101fa93
gmoinbong <vladosos.scherba@gmail.com>
2024-03-03 21:49:23 +0200

    streaming video format

--- aws/awsHandlers/getVideoHandler.go
+++ aws/awsHandlers/getVideoHandler.go
@@ -2,6 +2,8 @@ package awsHandlers
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"movie-service/aws/awsOperations"
 	"net/http"
 	"os"
@@ -10,11 +12,27 @@ import (
 func HandleVideoRequest(w http.ResponseWriter, r *http.Request, folderName string) {
 	region := os.Getenv("REGION")
 	bucketName := os.Getenv("BUCKET_NAME")
-	videoName := r.URL.Path[len("/"+folderName+"/"):]
+	videoName := r.URL.Path[len(folderName):]
 
 	videoURL, err := awsOperations.GetVideoURL(bucketName, region, folderName, videoName)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("unable to get video URL: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	resp, err := http.Get(videoURL)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("unable to open link: %v", err), http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	w.Header().Set("Content-Type", "video/mp4")
+	w.Header().Set("Content-Length", resp.Header.Get("Content-Length"))
+
+	_, err = io.Copy(w, resp.Body)
+	if err != nil {
+		log.Printf("error streaming vide: %v", err)
 		return
 	}
 
--- aws/awsOperations/getVideoURL.go
+++ aws/awsOperations/getVideoURL.go
@@ -6,7 +6,7 @@ import (
 
 func GetVideoURL(bucketName, region, folderName, videoName string) (string, error) {
 
-	videoURL := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s/%s", bucketName, region, folderName, videoName)
+	videoURL := fmt.Sprintf("https://%s.s3.%s.amazonaws.com%s%s", bucketName, region, folderName, videoName)
 
 	return videoURL, nil
 }
